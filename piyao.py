0400f9aba949a74acea183688b94c5dadaeaa589
ko_neath <ko_neath@outlook.com>
2019-07-16 15:37:08 +0800

    fixed

--- piyao.py
+++ piyao.py
@@ -6,7 +6,7 @@ from lxml import etree
 
 
 chrome_options = Options()
-#chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless')
 driver = webdriver.Chrome(chrome_options=chrome_options)
 
 
