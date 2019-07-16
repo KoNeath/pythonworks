381e9d32ee6cf06f82bd7fe3694fb01b4053f521
KoNeath <50345895+KoNeath@users.noreply.github.com>
2019-07-16 11:41:27 +0800

    Add files via upload

--- piyao.py
+++ piyao.py
@@ -1,13 +1,40 @@
-import requests
-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys  
+from selenium.webdriver.chrome.options import Options
+import time
 from lxml import etree
 
 
-request.header={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) ' 
-                         'Chrome/51.0.2704.63 Safari/537.36'}
+chrome_options = Options()
+#chrome_options.add_argument('--headless')
+driver = webdriver.Chrome(chrome_options=chrome_options)
 
-response=requests.get('https://piyao.sina.cn/')
 
-html=etree.HTML(response.content)
+driver.get("https://piyao.sina.cn/")
+time.sleep(1)
 
-date=html.xpath('')
+for i in range(0,30):#模拟滚轮下拉，生成页面，平均每次生成3天的新闻
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+    i += 1
+    time.sleep(2) 
+
+
+html = driver.find_elements_by_xpath("//div[@class='left_title']")
+fire=driver.find_elements_by_xpath("//div[@class='comment_text']")
+title=[]
+com=[]
+for a in html:
+    title.append(a.text)
+for b in fire:
+    com.append(int(b.text))
+item=zip(title,com)
+high=sorted(item,key=lambda x:x[1])
+print("本季最热门:\t")
+for a in high[-1:-11:-1]:
+    print(a[0],'\t',"评论数:",a[1],'\n')
+    
+
+
+
+
+
